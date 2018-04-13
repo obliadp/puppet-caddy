ae82a5b4ee76ce7e97938a8955b11e8f26052a51
Ørjan Ommundsen <orjanom@gmail.com>
2018-04-13 16:08:36 +0200

    caddy license

--- manifests/package.pp
+++ manifests/package.pp
@@ -13,7 +13,7 @@ class caddy::package inherits caddy {
   $caddy_dl_dir ="-o ${caddy::params::caddy_tmp_dir}/caddy_linux_${$caddy::params::arch}_custom.tar.gz"
 
   exec { 'install caddy':
-    command => "curl ${caddy_dl_dir} ${caddy_url}",
+    command => "curl ${caddy_dl_dir} \"${caddy_url}\"",
     creates => "${caddy::install_path}/caddy",
   }
 
